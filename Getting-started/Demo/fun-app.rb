ae63b6230f25992fa972c90c532a5bb0fd600844
Brian Almeida <73912537+Brianalmeida@users.noreply.github.com>
2024-03-11 17:21:03 -0400

    Create fun-app.rb

---
+++ Getting-started/Demo/fun-app.rb
@@ -0,0 +1,22 @@
+class Blurbalizer
+  def initialize(title)
+    @title		= title
+    @blurbs		= []
+  end
+  
+  def add_a_blurb(mood, content)
+    # The
+    @blurbs << Blurb.new(mood, content)
+  end
+  
+  def show_timeline
+    puts "Blurbify: #{@title} has #{@blurbs.count} Blurbs"
+    
+    @blurbs.sort_by { |t|
+      t.time
+      }.reverse.each { |t|
+        puts "#{t.content.ljust(40)}"} #{t.time}"
+	end
+end
+
+myapp = Blurbalizer.new "The Big Blurb Theory"
