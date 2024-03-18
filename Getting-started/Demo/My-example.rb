298069b5574c5b889ada26febe2ccf7f53df683d
Brian Almeida <73912537+Brianalmeida@users.noreply.github.com>
2024-03-18 09:23:39 -0400

    Create My-example.rb

---
+++ Getting-started/Demo/My-example.rb
@@ -0,0 +1,43 @@
+class Blurb
+  attr_accessor :content, :time, :mood
+
+  def initialize(mood, content="")
+    @time   = Time.now
+    @content = content[0..39]
+    @mood   = mood
+  end
+
+  def moodify
+    if @mood == :sad
+      return ":("
+    elsif @mood == :happy
+      return ":)"
+    elsif @mood == :meh
+      return ":|"
+    end
+    ":D"
+  end
+end
+
+class Blurbalizer
+  def initialize(title)
+    @title = title
+    @blurbs = []
+  end
+
+  def add_a_blurb(mood, content)
+    @blurbs << Blurb.new(mood, content)
+  end
+
+  def show_timeline
+    puts "Blurbalizer: #{@title} has #{@blurbs.count} Blurbs"
+
+    @blurbs.sort_by { |t|
+      t.time
+    }.reverse.each { |t|
+      puts "#{t.content.ljust(40)} #{t.time}"
+    }
+  end
+end
+
+myapp.show_timeline
